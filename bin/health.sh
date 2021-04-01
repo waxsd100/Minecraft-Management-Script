38b33acebd52a033f27bbe1c065d9da19668246b
waxsd100 <wakokara@wax100.io>
2021-04-02 03:45:15 +0900

    実行用Shellファイルを作成して設置するように変更 Server名がminecraft-〇〇固定値になっていたのを可変にできるように変更

--- bin/health.sh
+++ bin/health.sh
@@ -27,10 +27,12 @@
 # GitHub: waxsd100
 #
 
+VERSION="0.0.2"
 exec {lock_fd}< "$0"
 flock --nonblock ${lock_fd} || echo "[ERROR] Duplicate startup"
 cd "${0%/*}" > /dev/null 2>&1 || :
 declare -A WATCH_PROCESS;
+declare -A EXEC_COMMAND;
 
 readonly ME_FILE=$(basename $0)
 readonly SCRIPT_DIR=$(cd -- "$(dirname -- "$1")" && pwd)
@@ -41,10 +43,10 @@ readonly LOG_DIR="$(cd .. "$(dirname -- "$1")" && pwd)/log/"
 readonly LOCAL_IP=`ip -f inet -o addr show eth0|cut -d\  -f 7 | cut -d/ -f 1`
 
 # 表示設定
-readonly RESET=$'\e[0m'
 readonly BOLD=$'\e[1m'
 readonly RED=$'\e[1;31m'
 readonly GREEN=$'\e[1;32m'
+readonly RESET=$'\e[0m'
 readonly YMD=$(date '+%y/%m/%d %H:%M:%S')
 
 
@@ -98,31 +100,45 @@ screen_sender(){
 start(){
   # $1 screenName
   # $2 shellCommand
+  screen_name="$1"
+  target_dir="$2"
+  screen_exec=`echo "screen -AmdS ${SCREEN_PREFIX}-${screen_name} ${EXEC_COMMAND[$1]}"`
+
+  if [ -d "$target_dir" ]; then
+cat <<EOF > "$target_dir/run.sh"
+#!/bin/sh
+cd "${0%/*}" > /dev/null 2>&1
+$screen_exec
+#Version: $VERSION
+EOF
+  fi
+
   exitCode=0
   PROC_COUNT=`ps -ef | grep $proc_screen | grep -v grep | wc -l`
   if [ $PROC_COUNT = 0 ]; then
-    as_user "/bin/sh $2" || exitCode=$?
+    as_user "/bin/sh $target_dir/run.sh" || exitCode=$?
     if [ "$exitCode" = "0" ]; then
-      echo "[${YMD}] $1 Up"
-      send_discord "$1 Server Start" "${OUT}" "${LOCAL_IP}" "0x2ECC71"
+      echo "[${YMD}] [$screen_name] Up"
+      send_discord "[$screen_name] Server Start" "${OUT}" "${LOCAL_IP}" "0x2ECC71"
     else
-      echo "[${YMD}] $1 Up Oops"
-      send_discord "$1 Server Start Oops..." "${OUT}" "${LOCAL_IP}" "0x2ECC71"
+      echo "[${YMD}] [$screen_name] Up Oops"
+      send_discord "[$screen_name] Server Start Oops..." "${OUT}" "${LOCAL_IP}" "0x2ECC71"
     fi
   else
-    echo "[${YMD}] $1 is up and running"
-    send_discord "$1 is up and running..." "${OUT}" "${LOCAL_IP}" "0x2ECC71"
+    echo "[${YMD}] [$screen_name] is up and running"
+    send_discord "[$screen_name] is up and running..." "${OUT}" "${LOCAL_IP}" "0x2ECC71"
   fi
 }
 
 stop(){
   # $1 screenName
   # $2 ScreenCommand
-  screen_sender $1 $STOP_COMMAND
+  screen_name="${SCREEN_PREFIX}-$1"
+  screen_sender $screen_name $STOP_COMMAND
   if [ $? = 0 ]; then
-    OUT=`echo "[${YMD}] $1 Down"`
+    OUT=`echo "[${YMD}] [$1] Down"`
   else
-    OUT=`echo "[${YMD}] $1 Down Oops"`
+    OUT=`echo "[${YMD}] [$1] Down Oops"`
     for pid in `screen -list | grep $1 | cut -f1 -d'.' | sed 's/\W//g'`
     do
       echo "${pid} killed"
@@ -180,24 +196,25 @@ count_wait(){
 mc_check(){
   for proc_screen in ${!WATCH_PROCESS[@]};
   do
+    screen_name="${SCREEN_PREFIX}-$proc_screen"
     #監視するプロセスが何個起動しているかカウントする
-    PROC_COUNT=$(ps -ef | grep $proc_screen | grep -v grep | wc -l)
+    PROC_COUNT=$(ps -ef | grep $screen_name | grep -v grep | wc -l)
 
     # 監視するプロセスが0個場合に、処理を分岐する
     if [ $PROC_COUNT = 0 ]; then
     # 0の場合は、サービスが停止しているので起動する
-      echo "[${YMD}] $proc_screen Dead"
+      echo "[${YMD}] $screen_name Dead"
       mc_start
 
     elif [ $PROC_COUNT -ge 2 ]; then
     # 1以上の場合は、サービスが過剰に起動しているので再起動する
-      echo "[${YMD}] $proc_screen Over Running"
+      echo "[${YMD}] $screen_name Over Running"
       # カウントダウン後 Stop / Start を行う
       mc_restart 10 "§cプロセス異常を検知しました。" &
       wait
     else
     # サービス起動中
-      echo "[${YMD}] $proc_screen Alive"
+      echo "[${YMD}] $screen_name Alive"
     fi
   done
   # echo -1000 > "/proc/`pidof java`/oom_score_adj"
@@ -210,7 +227,8 @@ mc_start(){
   jobsCron true
   for proc_screen in ${!WATCH_PROCESS[@]};
   do
-    start $proc_screen ${WATCH_PROCESS[$proc_screen]}
+    start "${proc_screen}" "${WATCH_PROCESS[$proc_screen]}"
+#    start $proc_screen ${WATCH_PROCESS[$proc_screen]}
   done
 }
 
@@ -284,7 +302,7 @@ jobsCron(){
   EXEC_SHELL="/bin/sh ${SCRIPT_DIR}/${ME_FILE}"
   OUTPUT_LOG="${LOG_DIR}${LOG_FILE_NAME} 2>&1"
 
-  CRON_TAG="### Minecraft HealthCheck Cron ${ME_FILE} ###"
+  CRON_TAG="### Minecraft HealthCheck Version: $VERSION Cron ${ME_FILE} ###"
   BACKUP_CRON="0 * * * * ${EXEC_SHELL} backup >> ${OUTPUT_LOG}"
   CHECK_CRON="* * * * * ${EXEC_SHELL} check >> ${OUTPUT_LOG}"
   LOG_ROTATE="@daily find ${LOG_DIR}/ -name '*.log' -mtime +${LOG_LEAVE_DAYS} -delete"
