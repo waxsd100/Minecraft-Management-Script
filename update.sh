d8cd9e4659dd7e5e77c667b90850a6fbad31b4fe
waxsd100 <wakokara@wax100.io>
2021-10-02 22:55:38 +0900

    Update用Scriptを追加

---
+++ update.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+temp_dir=$(mktemp -d)
+cd "${0%/*}" >/dev/null 2>&1 || :
+dir=`pwd`
+
+\cp -rf $dir/log/ "$temp_dir"
+\cp -f $dir/config.inc "$temp_dir"
+
+git pull
+git fetch origin
+git reset --hard origin/master
+
+\cp -rf "$temp_dir"/log/* $dir/
+\cp -f  "$temp_dir"/config.inc $dir/
