c3cdab56a10178b71bfa1bb171750d94b5755ce6
waxsd100 <wakokara@wax100.io>
2021-10-02 23:14:42 +0900

    Bugfix

--- update.sh
+++ update.sh
@@ -5,9 +5,11 @@ dir=`pwd`
 
 \cp -rf $dir/log/ "$temp_dir"
 \cp -f $dir/config.inc "$temp_dir"
+echo "Backup $dir"
 
 git fetch origin
 git reset --hard origin/master
 
 \cp -rf "$temp_dir"/log/* $dir/log/
 \cp -f  "$temp_dir"/config.inc $dir/
+echo "Restore $temp_dir"
