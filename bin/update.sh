670b07585cf30dd6f5309ca39ee294f45584e4ca
waxsd100 <wakokara@wax100.io>
2021-10-12 01:23:41 +0900

    Create update.sh

---
+++ bin/update.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+temp_dir=$(mktemp -d)
+cd "${0%/*}" >/dev/null 2>&1 || :
+cd ../
+dir=`pwd`
+
+\cp -rf $dir/log/ "$temp_dir"
+\cp -f $dir/config.inc "$temp_dir"
+echo "Backup $dir"
+
+git fetch origin
+git reset --hard origin/master
+
+\cp -rf "$temp_dir"/log/* $dir/log/
+\cp -f  "$temp_dir"/config.inc $dir/
+echo "Restore $temp_dir"
+
+/bin/sh bin/health.sh uninstall
+/bin/sh bin/health.sh install
+echo "Crontab reinstall"
